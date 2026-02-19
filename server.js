6f5750004bc45ce686b939a41d8d64f8f13fab75
Teja200807 <tejasrisai15@gmail.com>
2026-02-19 12:54:55 +0530

    Fix server.js to include missing PORT definition

--- server.js
+++ server.js
@@ -46,6 +46,10 @@ app.get("/", (req, res) => {
   res.send("Backend is running successfully 🚀");
 });
 
+const PORT = process.env.PORT || 3000;
+
 app.listen(PORT, () => {
   console.log("Server running on port " + PORT);
 });
+
+
